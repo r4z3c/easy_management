78ef0881302fa3ab6c6211b489aba75fe4bf1a31
r4z3c <cezar.almeidajr@gmail.com>
2017-05-23 12:44:05 -0300

    - Refactoring registry/record and helpers/model_helper

--- lib/easy_management/helpers/model_helper.rb
+++ lib/easy_management/helpers/model_helper.rb
@@ -3,15 +3,16 @@ module EasyManagement
 
     class ModelHelper
 
-      attr_accessor :name
+      attr_accessor :name, :manager, :controller
 
       def initialize(name)
-        self.name = name.singularize.downcase
+        self.name = name.to_s.singularize.downcase
+        setup
       end
 
       def namespace
         chain = underscored_name.split('/')[0..-2]
-        chain.empty? ? nil : chain.join('/').classify
+        chain.empty? ? nil : chain.join('/').camelize
       end
 
       def controller_class
@@ -23,7 +24,7 @@ module EasyManagement
       end
 
       def model_class
-        underscored_name.split('/').last.classify
+        underscored_name.split('/').last.camelize
       end
 
       def controller_path
@@ -56,6 +57,51 @@ module EasyManagement
 
       def app_path
         'app'
+      end
+
+      protected
+
+      def setup
+        @manager ||= create_manager
+        @controller ||= create_controller
+      end
+
+      def create_manager
+        manager_class_str = "#{namespace}::#{self.manager_class}"
+
+        manager_class = create_class manager_class_str, EasyManagement::Managers::BaseManager
+
+        model_class = underscored_name.classify.constantize
+
+        manager_class.send :define_method, :model do
+          model_class
+        end
+
+        model_class_sym = self.model_class.downcase.to_sym
+
+        manager_class.send :define_method, :model_name do
+          model_class_sym
+        end
+
+        manager_class
+      end
+
+      def create_controller
+        controller_class_str = "#{namespace}::#{self.controller_class}"
+
+        controller_class = create_class controller_class_str, EasyManagement::Controllers::BaseController
+
+        manager_class = @manager
+
+        controller_class.send :define_method, :manager do
+          manager_class
+        end
+
+        controller_class
+      end
+
+      def create_class(full_name, superclass)
+        ModelBuilder::ClassBuilder.build full_name, superclass: superclass
       end
 
     end
