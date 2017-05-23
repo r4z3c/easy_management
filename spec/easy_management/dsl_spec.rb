d2e77a7f4f4ca20c6d5f7bdf18f90e8b60ed0042
r4z3c <cezar.almeidajr@gmail.com>
2017-05-23 12:44:46 -0300

    - Adding spec for dsl and model_helper

---
+++ spec/easy_management/dsl_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'easy_management/testing/support/active_record/dummies_controller'
+require 'easy_management/testing/support/active_record/dummies_manager'
+require 'easy_management/testing/support/active_record/models'
+
+EasyManagement::Testing::Support::ActiveRecord::Models.build_dummy_model
+
+describe EasyManagement::Dsl do
+
+  let(:dsl) { EasyManagement::Dsl }
+
+  let(:record) { EasyManagement::Registry::Repository.singleton.records.first }
+
+  let(:target) { 'easy_management/testing/support/active_record/dummy' }
+
+  describe '.configure' do
+
+    before { dsl.configure { manage 'easy_management/testing/support/active_record/dummy' } }
+
+    it { expect(record).to be }
+
+    it { expect(record.target).to eq target }
+
+    it { expect(record.options).to eq Hash.new }
+
+    it { expect(record.helper.name).to eq target }
+
+    it { expect(record.manager).to eq EasyManagement::Testing::Support::ActiveRecord::DummiesManager }
+
+    it { expect(record.controller).to eq EasyManagement::Testing::Support::ActiveRecord::DummiesController }
+
+    it { expect(record.controller.new.send :manager).to eq EasyManagement::Testing::Support::ActiveRecord::DummiesManager }
+
+  end
+
+end
---
+++ spec/easy_management/helpers/model_helper_spec.rb
@@ -0,0 +1,126 @@
+require 'spec_helper'
+require 'easy_management/testing/support/active_record/models'
+require 'easy_management/testing/support/active_record/dummies_manager'
+require 'easy_management/testing/support/active_record/dummies_controller'
+
+EasyManagement::Testing::Support::ActiveRecord::Models.build_dummy_model
+
+describe EasyManagement::Helpers::ModelHelper do
+
+  let(:helper) { EasyManagement::Helpers::ModelHelper.new 'easy_management/testing/support/active_record/dummy' }
+
+  describe '#controller' do
+
+    subject { helper.controller }
+
+    it { is_expected.to eq EasyManagement::Testing::Support::ActiveRecord::DummiesController }
+
+    it { expect(subject.new.send :manager).to eq EasyManagement::Testing::Support::ActiveRecord::DummiesManager }
+
+  end
+
+  describe '#manager' do
+
+    subject { helper.manager }
+
+    it { is_expected.to eq EasyManagement::Testing::Support::ActiveRecord::DummiesManager }
+
+  end
+
+  describe '#namespace' do
+
+    subject { helper.namespace }
+
+    it { is_expected.to eq 'EasyManagement::Testing::Support::ActiveRecord' }
+
+  end
+
+  describe '#controller_class' do
+
+    subject { helper.controller_class }
+
+    it { is_expected.to eq 'DummiesController' }
+
+  end
+
+  describe '#manager_class' do
+
+    subject { helper.manager_class }
+
+    it { is_expected.to eq 'DummiesManager' }
+
+  end
+
+  describe '#model_class' do
+
+    subject { helper.model_class }
+
+    it { is_expected.to eq 'Dummy' }
+
+  end
+
+  describe '#controller_path' do
+
+    subject { helper.controller_path }
+
+    it { is_expected.to eq 'app/controllers/easy_management/testing/support/active_record/dummies_controller.rb' }
+
+  end
+
+  describe '#manager_path' do
+
+    subject { helper.manager_path }
+
+    it { is_expected.to eq 'app/managers/easy_management/testing/support/active_record/dummies_manager.rb' }
+
+  end
+
+  describe '#model_path' do
+
+    subject { helper.model_path }
+
+    it { is_expected.to eq 'app/models/easy_management/testing/support/active_record/dummy.rb' }
+
+  end
+
+  describe '#underscored_name' do
+
+    subject { helper.underscored_name }
+
+    it { is_expected.to eq 'easy_management/testing/support/active_record/dummy' }
+
+  end
+
+  describe '#controllers_path' do
+
+    subject { helper.controllers_path }
+
+    it { is_expected.to eq 'app/controllers' }
+
+  end
+
+  describe '#managers_path' do
+
+    subject { helper.managers_path }
+
+    it { is_expected.to eq 'app/managers' }
+
+  end
+
+  describe '#models_path' do
+
+    subject { helper.models_path }
+
+    it { is_expected.to eq 'app/models' }
+
+  end
+
+  describe '#app_path' do
+
+    subject { helper.app_path }
+
+    it { is_expected.to eq 'app' }
+
+  end
+
+end
